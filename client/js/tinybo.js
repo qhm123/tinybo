4d6bb77d97ba45e50d612aafbdf762a6d68f6411
qhm123 <qhm123@gmail.com>
2012-05-24 16:24:36 +0800

    jqm ui

--- client/js/tinybo.js
+++ client/js/tinybo.js
@@ -1,146 +1,134 @@
 function login() {
-	sina.weibo.init({
-		appKey: "19CDAEC7FED64A40458D5817820E894B2B33A1CA68520B51",
-		appSecret: "BF474EF214B506A9E99C7F69B28E2E28E610B137F4666588F0FF8E8AF65E7D7045A3ECC5157059B5",
-		redirectUrl: "http://mobilecloudweibo.sinaapp.com"
-	},
-	function(response) {
-		alert("init: " + response);
+    sina.weibo.init({
+        appKey: "19CDAEC7FED64A40458D5817820E894B2B33A1CA68520B51",
+        appSecret: "BF474EF214B506A9E99C7F69B28E2E28E610B137F4666588F0FF8E8AF65E7D7045A3ECC5157059B5",
+        redirectUrl: "http://mobilecloudweibo.sinaapp.com"
+    }, function(response) {
+        alert("init: " + response);
 
-		sina.weibo.login(function(access_token, expires_in) {
-			if (access_token && expires_in) {
-				alert('logged in');
-				localStorage.setItem('access_t', access_token);
-			} else {
-				alert('not logged in');
-			}
-		});
+        sina.weibo.login(function(access_token, expires_in) {
+            if (access_token && expires_in) {
+                alert('logged in');
+                localStorage.setItem('access_t', access_token);
+            } else {
+                alert('not logged in');
+            }
+        });
 
-	},
-	function(msg) {
-		alert(msg);
-	});
+    }, function(msg) {
+        alert(msg);
+    });
 }
 
 function refresh() {
-	var statusesView = new StatusesView();
+    var statusesView = new StatusesView();
 }
 
 function deviceReady() {
 
-	(function() {
+    (function() {
 
-		alert("device ready");
+        $.mobile.initializePage();
 
-		var Status = Backbone.Model.extend({
+        var Status = Backbone.Model.extend({
 
-			defaults: {
-				created_at: new Date(),
-				id: 0,
-				text: "",
-				source: "新浪微博",
-				reposts_count: 0,
-				comments_count: 0
-			},
+            defaults: {
+                created_at: new Date(),
+                id: 0,
+                text: "",
+                source: "新浪微博",
+                reposts_count: 0,
+                comments_count: 0
+            },
 
-			initialize: function() {}
-		});
+            initialize: function() {}
+        });
 
-		var Statuses = Backbone.Collection.extend({
+        var Statuses = Backbone.Collection.extend({
 
-			model: Status,
+            model: Status,
 
-			url: 'https://api.weibo.com/2/statuses/home_timeline.json',
+            url: 'https://api.weibo.com/2/statuses/home_timeline.json',
 
-			sync: function(method, model, options) {
-				sina.weibo.get("https://api.weibo.com/2/statuses/home_timeline.json", {
-					access_token: localStorage.getItem('access_t')
-				},
-				function(response) {
-					options.success(JSON.parse(response));
-					console.log("sync success");
-					//var data = stringToJSON(response);
-					//alert('statuses[0].text: ' + data.statuses[0].text);
-				},
-				function(response) {
-					console.log('error: ' + response);
-				});
-				/*
-      var that = this;
-      var params = _.extend({
-        type: 'GET',
-        dataType: 'jsonp',
-        url: that.url,
-        processData: false
-      }, options);
+            sync: function(method, model, options) {
+                sina.weibo.get("https://api.weibo.com/2/statuses/home_timeline.json", {
+                    access_token: localStorage.getItem('access_t')
+                }, function(response) {
+                    options.success(JSON.parse(response));
+                    console.log("sync success");
+                    //var data = stringToJSON(response);
+                    //alert('statuses[0].text: ' + data.statuses[0].text);
+                }, function(response) {
+                    console.log('error: ' + response);
+                });
+            },
 
-      return $.ajax(params);
-      */
-			},
+            parse: function(response) {
+                console.log(JSON.stringify(response));
+                return response.statuses;
+            }
+            //localStorage: new Store("statuses-backbone")
+        });
 
-			parse: function(response) {
-				console.log(JSON.stringify(response));
-				return response.statuses;
-			}
-			//localStorage: new Store("statuses-backbone")
-		});
+        var statuses = new Statuses();
 
-		var statuses = new Statuses();
+        var StatusView = Backbone.View.extend({
 
-		var StatusView = Backbone.View.extend({
+            tagName: "li",
 
-			tagName: "li",
+            template: _.template($('#status-item-template').html()),
 
-			template: _.template($('#status-item-template').html()),
+            events: {},
 
-			events: {},
+            initialize: function() {
+                _.bindAll(this, 'render');
+            },
 
-			initialize: function() {
-				_.bindAll(this, 'render');
-			},
+            render: function() {
+                $(this.el).html(this.template(this.model.toJSON()));
+                return this;
+            }
+        });
 
-			render: function() {
-				$(this.el).html(this.template(this.model.toJSON()));
-				return this;
-			}
-		});
+        var StatusesView = Backbone.View.extend({
 
-		var StatusesView = Backbone.View.extend({
+            el: $('#statuses'),
 
-			el: $('#statuses'),
+            initialize: function() {
+                _.bindAll(this, 'render', 'addOne', 'addAll');
 
-			initialize: function() {
-				_.bindAll(this, 'render', 'addOne', 'addAll');
+                statuses.bind('add', this.addOne);
+                statuses.bind('reset', this.addAll);
+                statuses.bind('all', this.render);
 
-				statuses.bind('add', this.addOne);
-				statuses.bind('reset', this.addAll);
-				statuses.bind('all', this.render);
+                statuses.fetch();
+            },
 
-				statuses.fetch();
-			},
+            render: function() {
+                this.$('#status-list').listview('refresh');
+            },
 
-			render: function() {},
+            addOne: function(status) {
+                var view = new StatusView({
+                    model: status
+                });
+                this.$('#status-list').append(view.render().el);
+            },
 
-			addOne: function(status) {
-				var view = new StatusView({
-					model: status
-				});
-				this.$('#status-list').append(view.render().el);
-			},
+            addAll: function() {
+                console.log('addAll: ' + statuses.length);
+                statuses.each(this.addOne);
+            }
+        });
 
-			addAll: function() {
-        console.log('addAll: ' + statuses.length);
-				statuses.each(this.addOne);
-			}
-		});
+        var statusesView = new StatusesView();
 
-		var statusesView = new StatusesView();
-
-	})();
+    })();
 
 }
 
 document.addEventListener('deviceready', function() {
-	deviceReady();
-},
-false);
+    deviceReady();
+}, false);
+
+//$(function(){ deviceReady(); });
